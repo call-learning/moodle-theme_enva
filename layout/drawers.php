c6d7b72591bb2e257808111d418abe0e1a693696
Laurent David <lmedavid@gmail.com>
2026-02-02 20:33:10 +0100

    Add return url to main pages

--- layout/drawers.php
+++ layout/drawers.php
@@ -101,7 +101,13 @@ $templatecontext = [
     'hasregionmainsettingsmenu' => !empty($regionmainsettingsmenu),
     'overflow' => $overflow,
     'headercontent' => $headercontent,
-    'addblockbutton' => $addblockbutton
+    'addblockbutton' => $addblockbutton,
 ];
 
 echo $OUTPUT->render_from_template('theme_enva/layout/drawers', $templatecontext);
+
+global $SESSION;
+// Make sure if we use the login form and $SESSION->wantsurl is not set, then set it here.
+if ((!isloggedin() or isguestuser()) && empty($SESSION->wantsurl)) {
+    $SESSION->wantsurl = qualified_me();
+}
